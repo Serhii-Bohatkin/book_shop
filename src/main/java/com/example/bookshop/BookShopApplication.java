eaa365f82b0dd7fbc6d36f5af91eb71e082ca7d2
Sergey Bogatkin <daniltisenko22@gmail.com>
2023-10-14 17:14:56 +0300

    Removed BookRepositoryImpl class, BookRepository now inherits from JpaRepository. Implemented soft deletion for a Book. Added updateBook() and deleteBook() methods to BookController. Added update() and deleteById() methods to BookServiceImpl

--- src/main/java/com/example/bookshop/BookShopApplication.java
+++ src/main/java/com/example/bookshop/BookShopApplication.java
@@ -1,17 +1,10 @@
 package com.example.bookshop;
 
-import com.example.bookshop.mapper.BookMapper;
-import com.example.bookshop.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookShopApplication {
-    @Autowired
-    private BookService bookService;
-    @Autowired
-    private BookMapper bookMapper;
 
     public static void main(String[] args) {
         SpringApplication.run(BookShopApplication.class, args);
--- src/main/java/com/example/bookshop/controller/BookController.java
+++ src/main/java/com/example/bookshop/controller/BookController.java
@@ -5,11 +5,15 @@ import com.example.bookshop.dto.CreateBookRequestDto;
 import com.example.bookshop.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -25,11 +29,22 @@ public class BookController {
 
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
-        return bookService.findBookById(id);
+        return bookService.findById(id);
     }
 
     @PostMapping
     public BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBook(@RequestBody CreateBookRequestDto requestDto, @PathVariable Long id) {
+        return bookService.update(requestDto, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteById(id);
     }
 }
