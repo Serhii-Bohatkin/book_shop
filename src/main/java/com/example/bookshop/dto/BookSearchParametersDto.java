231b8211992d81a6856ce6afc899e5c2a1ee894f
Sergey Bogatkin <131189925+Sergey-Bogatkin@users.noreply.github.com>
2023-10-23 14:35:18 +0300

    Revert " Book shop pr#3 (#3)" (#4)
    This reverts commit b11b67ed42bff65889e24606941b19cd0922a82f.

--- src/main/java/com/example/bookshop/dto/BookSearchParametersDto.java
+++
@@ -1,5 +0,0 @@
-package com.example.bookshop.dto;
-
-public record BookSearchParametersDto(String[] titles, String[] authors, String[] isbns,
-                                      String[] minPrice, String[] maxPrice, String[] descriptions) {
-}
--- src/main/java/com/example/bookshop/service/BookService.java
+++ src/main/java/com/example/bookshop/service/BookService.java
@@ -1,7 +1,6 @@
 package com.example.bookshop.service;
 
 import com.example.bookshop.dto.BookDto;
-import com.example.bookshop.dto.BookSearchParametersDto;
 import com.example.bookshop.dto.CreateBookRequestDto;
 import java.util.List;
 
@@ -10,11 +9,5 @@ public interface BookService {
 
     List<BookDto> findAll();
 
-    BookDto findById(Long id);
-
-    BookDto update(CreateBookRequestDto requestDto, Long id);
-
-    void deleteById(Long id);
-
-    List<BookDto> search(BookSearchParametersDto searchParameters);
+    BookDto findBookById(Long id);
 }
