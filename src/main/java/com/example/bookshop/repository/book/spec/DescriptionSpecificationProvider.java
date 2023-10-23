b11b67ed42bff65889e24606941b19cd0922a82f
Sergey Bogatkin <131189925+Sergey-Bogatkin@users.noreply.github.com>
2023-10-23 14:06:00 +0300

    Book shop pr#3 (#3)
    * Added classes Book, Book RepositoryImpl,BookServiceImpl
    
    * Added classes EntityNotFoundException, BookDto, CreateBookRequestDto, BookMapper.
    Added BookController class with methods:
    getAll(), getBookById(), createBook().
    Added findBookById() method to BookService and BookRepository.
    Added Mapstruct
    
    * Deleted CommandLineRunner bean
    
    * Removed BookRepositoryImpl class, BookRepository now inherits from JpaRepository. Implemented soft deletion for a Book. Added updateBook() and deleteBook() methods to BookController. Added update() and deleteById() methods to BookServiceImpl
    
    * Added Liquibase support
    
    * Implemented book search using CreteriaAPI. Added classes BookSearchParametersDto, SpecificationBuilder, SpecificationProvider, SpecificationProviderManager, BookSpecificationProviderManager, BookSpecificationBuilder and book specifications
    
    * Added CustomGlobalExceptionHandler class. Added validation for CreateBookRequestDto class

---
+++ src/main/java/com/example/bookshop/repository/book/spec/DescriptionSpecificationProvider.java
@@ -0,0 +1,21 @@
+package com.example.bookshop.repository.book.spec;
+
+import com.example.bookshop.model.Book;
+import com.example.bookshop.repository.SpecificationProvider;
+import java.util.Arrays;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DescriptionSpecificationProvider implements SpecificationProvider<Book> {
+    @Override
+    public String getKey() {
+        return "description";
+    }
+
+    @Override
+    public Specification<Book> getSpecification(String[] params) {
+        return (root, query, criteriaBuilder)
+                -> root.get("description").in(Arrays.stream(params).toArray());
+    }
+}
