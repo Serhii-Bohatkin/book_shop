5ed74fe254ac19135bc514a155b4e21c1d004d9a
Sergey Bogatkin <131189925+Sergey-Bogatkin@users.noreply.github.com>
2023-10-26 19:36:44 +0300

    Revert "Book shop PR#4 (#5)"
    This reverts commit 79c97854e7d4eb7f8233c9e63c93de8e21864429.

--- src/main/java/com/example/bookshop/repository/book/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookshop.repository.book;
-
-import com.example.bookshop.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
-}
