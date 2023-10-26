e2b44b16ec04c5fa766d808b2829a6d929c9544f
Sergey Bogatkin <131189925+Sergey-Bogatkin@users.noreply.github.com>
2023-10-26 19:38:07 +0300

    Revert "Book shop PR#4 (#5)" (#7)
    This reverts commit 79c97854e7d4eb7f8233c9e63c93de8e21864429.

--- src/main/java/com/example/bookshop/service/BookServiceImpl.java
+++ src/main/java/com/example/bookshop/service/BookServiceImpl.java
@@ -1,28 +1,20 @@
 package com.example.bookshop.service;
 
 import com.example.bookshop.dto.BookDto;
-import com.example.bookshop.dto.BookSearchParametersDto;
 import com.example.bookshop.dto.CreateBookRequestDto;
 import com.example.bookshop.exception.EntityNotFoundException;
 import com.example.bookshop.mapper.BookMapper;
 import com.example.bookshop.model.Book;
-import com.example.bookshop.repository.book.BookRepository;
-import com.example.bookshop.repository.book.BookSpecificationBuilder;
+import com.example.bookshop.repository.BookRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
-    @Autowired
     private final BookRepository bookRepository;
-    @Autowired
     private final BookMapper bookMapper;
-    @Autowired
-    private final BookSpecificationBuilder bookSpecificationBuilder;
 
     @Override
     public BookDto save(CreateBookRequestDto requestDto) {
@@ -39,35 +31,9 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public BookDto findById(Long id) {
-        Book book = bookRepository.findById(id).orElseThrow(
+    public BookDto findBookById(Long id) {
+        Book book = bookRepository.findBookById(id).orElseThrow(
                 () -> new EntityNotFoundException("Can't get a book by id " + id));
         return bookMapper.toDto(book);
-    }
-
-    @Override
-    public BookDto update(CreateBookRequestDto requestDto, Long id) {
-        Book bookFromDB = bookRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException("Can't update a book with id " + id));
-        bookFromDB.setTitle(requestDto.getTitle());
-        bookFromDB.setAuthor(requestDto.getAuthor());
-        bookFromDB.setIsbn(requestDto.getIsbn());
-        bookFromDB.setPrice(requestDto.getPrice());
-        bookFromDB.setDescription(requestDto.getDescription());
-        bookFromDB.setCoverImage(requestDto.getCoverImage());
-        return bookMapper.toDto(bookRepository.save(bookFromDB));
-    }
-
-    @Override
-    public void deleteById(Long id) {
-        bookRepository.deleteById(id);
-    }
-
-    @Override
-    public List<BookDto> search(BookSearchParametersDto searchParameters) {
-        Specification<Book> bookSpecification = bookSpecificationBuilder.build(searchParameters);
-        return bookRepository.findAll(bookSpecification).stream()
-                .map(bookMapper::toDto)
-                .toList();
     }
 }
