75d86f11fdbdee48d9cb80fb06edffcf09479f39
Sergey Bogatkin <131189925+Sergey-Bogatkin@users.noreply.github.com>
2023-10-26 20:47:03 +0300

    Implemented book search using CreteriaAPI. Added classes BookSearchParametersDto, SpecificationBuilder, SpecificationProvider, SpecificationProviderManager, BookSpecificationProviderManager, BookSpecificationBuilder and book specifications (#8)

--- src/main/java/com/example/bookshop/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookshop.repository;
-
-import com.example.bookshop.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- src/main/java/com/example/bookshop/service/BookService.java
+++ src/main/java/com/example/bookshop/service/BookService.java
@@ -1,6 +1,7 @@
 package com.example.bookshop.service;
 
 import com.example.bookshop.dto.BookDto;
+import com.example.bookshop.dto.BookSearchParametersDto;
 import com.example.bookshop.dto.CreateBookRequestDto;
 import java.util.List;
 
@@ -14,4 +15,6 @@ public interface BookService {
     BookDto update(CreateBookRequestDto requestDto, Long id);
 
     void deleteById(Long id);
+
+    List<BookDto> search(BookSearchParametersDto searchParameters);
 }
