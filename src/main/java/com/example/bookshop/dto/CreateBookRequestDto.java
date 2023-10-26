8a985edbd72cd27fbbaeac100f9a8be1e0a04514
Sergey Bogatkin <daniltisenko22@gmail.com>
2023-10-26 21:48:46 +0300

    Added CustomGlobalExceptionHandler class. Added validation for CreateBookRequestDto class

--- src/main/java/com/example/bookshop/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/bookshop/dto/CreateBookRequestDto.java
@@ -1,14 +1,22 @@
 package com.example.bookshop.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    private Long id;
+    @NotEmpty
     private String title;
+    @NotEmpty
     private String author;
+    @NotEmpty
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
